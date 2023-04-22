8fc6e87a09ac2e16eb7a1e904d7e3b1217673f5b
rezaazimibar <www.beta04@gmail.com>
2023-04-22 10:46:38 +0430

    add tailwind and fonts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
+      <div >اپلیکیشن مدیریت مخاطبین</div>
+      <button className="bg-green-700 p-4">this is the button</button>
+      <span className="fa fa-user"></span>
+      <span className="fas fa-car"></span>
+      <i className="fas fa-users"></i>
+      <i className="fas fa-users"></i>
+      <i class="fas fa-person-booth text-8xl"></i>
     </div>
   );
 };
